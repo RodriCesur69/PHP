05657a0434fa7a13bcbe2d4d4c79f3af1d84d670
Rodrigo <wuinirealoffice@gmail.com>
2025-11-06 12:01:53 +0100

    primer commit

---
+++ PracticasIniciales/Ej1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$modulos = ["Bases de Datos", "Programación", "Sistemas Informáticos"];
+$modulos[2] = "FOL";
+// añadir algo mas al array
+$modulos[3] = "Lenguaje de Marcas";
+print_r($modulos);
+
+
+
+?>
+
+</body>
+</html>
---
+++ PracticasIniciales/Ej2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=º, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$usuarios = [
+    "nombre" => "yasin",
+    "correo" => "yasin@gmail.com",
+    "edad" => 25
+
+];
+echo $usuarios["nombre"];
+    ?>
+</body>
+</html>
